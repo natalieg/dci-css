f0132fbe9039a43b24f313d7207539895c2c8fc2
Natalie <natalie.gornicki@gmail.com>
2019-05-28 20:57:49 +0200

    history timeline

---
+++ w10/1-bootstrap-project/js/connection.js
@@ -0,0 +1,17 @@
+
+
+jQuery(document).ready(function () {
+  var connection1 = jQuery('#img1').connections({ to: '#img2' });
+  var connection2 = jQuery('#img2').connections({ to: '#img3' });
+  var connection3 = jQuery('#img3').connections({ to: '#img4' });
+  var connection4 = jQuery('#img4').connections({ to: '#img5' });
+
+  $(window).on('resize', function () {
+    connection1.connections('update');
+    connection2.connections('update');
+    connection3.connections('update');
+    connection4.connections('update');
+  });
+
+});
+
