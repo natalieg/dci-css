d874cd8944d27bdd42dd3dc9b4b720067b50300b
Natalie-Gornicki <natalie.gornicki@gmail.com>
2019-05-29 11:44:56 +0200

    nav change

--- w10/1-bootstrap-project/js/connection.js
+++ w10/1-bootstrap-project/js/connection.js
@@ -1,12 +1,14 @@
 
 
 jQuery(document).ready(function () {
+  var connection0 = jQuery('#text1').connections({ to: '#img1' });
   var connection1 = jQuery('#img1').connections({ to: '#img2' });
   var connection2 = jQuery('#img2').connections({ to: '#img3' });
   var connection3 = jQuery('#img3').connections({ to: '#img4' });
   var connection4 = jQuery('#img4').connections({ to: '#img5' });
 
   $(window).on('resize', function () {
+    connection0.connections('update');
     connection1.connections('update');
     connection2.connections('update');
     connection3.connections('update');
--- w10/1-bootstrap-project/js/script.js
+++ w10/1-bootstrap-project/js/script.js
@@ -1,6 +1,16 @@
+
+var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+
 // When the user scrolls down 50px from the top of the document, resize the header's font size
-window.onscroll = function () { scrollFunction() 
-   //
+window.onscroll = function () { 
+    scrollBackgroundNav();
+    // if (viewportWidth > 640) {
+    //     document.getElementById("header").style.height = "80vh";
+    //     scrollFunction(); 
+    // } else {
+    //     document.getElementById("header").style.height = "40vh";
+    //     console.log('Small viewport');
+    // }
 };
 
 function scrollFunction() {
@@ -8,5 +18,13 @@ function scrollFunction() {
         document.getElementById("header").style.height = "45vh";
     } else {
         document.getElementById("header").style.height = "80vh";
+    }
+}
+
+function scrollBackgroundNav() {
+    if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 50) {
+        document.getElementById("mainNav").style.backgroundColor = "#FFD038";
+    } else {
+        document.getElementById("mainNav").style.backgroundColor = "rgba(255, 255, 255, 0)";
     }
 }
