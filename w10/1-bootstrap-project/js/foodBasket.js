c27fe71d6e8d1779c0ec49b496ceb14ecdddebb0
Natalie-Gornicki <natalie.gornicki@gmail.com>
2019-05-29 14:50:05 +0200

    foodanimation

--- w10/1-bootstrap-project/js/foodBasket.js
+++ w10/1-bootstrap-project/js/foodBasket.js
@@ -1,14 +1,28 @@
 var basket = document.getElementById("basket");
 var apple = document.getElementById("apple");
+var barley = document.getElementById("barley");
+var originalPositionLeft = "35vw"
+var originalPositionTop = "220px"
 var active = false;
 
 basket.onclick = function(){
     console.log("hi");
     if(!active){
-        apple.style.transform = "translate(-220px, -20px)";
+        apple.style.top = "260px";
+        apple.style.left = "10vw";
+
+        barley.style.top = "90px";
+        barley.style.left = "5vw";
+
         active = true;
     } else {
-        apple.style.transform = "translate(0)";
+        resetPosition(apple);
+        resetPosition(barley);
         active = false;
     }
+}
+
+var resetPosition = function(item){
+    item.style.top = originalPositionTop;
+    item.style.left = originalPositionLeft;
 }
--- w10/1-bootstrap-project/js/header.js
+++ w10/1-bootstrap-project/js/header.js
@@ -2,30 +2,37 @@ var viewportWidth = document.documentElement.clientWidth;
 
 
 window.onload = function(){
-    changeHeight();
-    changeColor();
+    console.log("hy");
+    changeHeightAndColor();
 }
 
 window.onresize = function () {
-    changeHeight();
-    changeColor();
+    console.log("hy2");
+    changeHeightAndColor();
 };
 
+window.onscroll = function () {
+    if (viewportWidth > 768) {
+        scrollBackgroundNav();
+    } 
+};
 
-function changeHeight () {
+function scrollBackgroundNav() {
+    if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 50) {
+        document.getElementById("mainNav").style.backgroundColor = "#FFD038";
+    } else {
+        document.getElementById("mainNav").style.backgroundColor = "rgba(255, 255, 255, 0)";
+    }
+}
+
+
+function changeHeightAndColor () {
     viewportWidth = document.documentElement.clientWidth;
     if (viewportWidth > 768) {
         document.getElementById("header").style.height = "80vh";
-    } else {
-        document.getElementById("header").style.height = "40vh";
-    }
-};
-
-function changeColor() {
-    viewportWidth = document.documentElement.clientWidth;
-    if (viewportWidth > 768) {
         document.getElementById("mainNav").style.backgroundColor = "rgba(255, 255, 255, 0)";
     } else {
+        document.getElementById("header").style.height = "40vh";
         document.getElementById("mainNav").style.backgroundColor = "#FFD038";
     }
 };
