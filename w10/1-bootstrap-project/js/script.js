d11507c155fd3dac00bdbd0abfa479747ffab629
Natalie-Gornicki <natalie.gornicki@gmail.com>
2019-05-29 16:14:08 +0200

    nav fix

--- w10/1-bootstrap-project/js/script.js
+++ w10/1-bootstrap-project/js/script.js
@@ -19,11 +19,13 @@ window.onresize = function () {
 
 function scrollBackgroundNav() {
     viewportWidth = document.documentElement.clientWidth;
-    if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 50) {
-        document.getElementById("mainNav").style.backgroundColor = "#FFD038";
-    } else {
-        document.getElementById("mainNav").style.backgroundColor = "rgba(255, 255, 255, 0)";
-    }
+    if (viewportWidth > 768) {
+        if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 50) {
+            document.getElementById("mainNav").style.backgroundColor = "#FFD038";
+        } else {
+            document.getElementById("mainNav").style.backgroundColor = "rgba(255, 255, 255, 0)";
+        }
+    } 
 }
 
 function changeColor() {
